6e6d08dd73cef1f72b0b4db21ef351c06cb9ca06
ccnmagnoo <crcamposn@gmail.com>
2023-08-28 09:45:55 -0400

    sass config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+const nextConfig = {
+  sassOptions: { includePaths: [path.join(__dirname, 'styles')] },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
