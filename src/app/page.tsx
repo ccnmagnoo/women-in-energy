b37cdbd5d491c91cd681758fbcc45b09f261c9a9
ccnmagnoo <crcamposn@gmail.com>
2023-08-31 08:09:23 -0400

    animation change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image';
+import woman from '../../public/woman-in-energy-css.svg';
+
 function Home() {
   return (
     <>
       <header>mujeres instaladoras</header>
-      <main>content</main>
+      <main>
+        <Image src={woman} alt='' height={500} />
+      </main>
       <footer>bottom bar</footer>
     </>
   );
