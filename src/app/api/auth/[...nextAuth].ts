5c4da54c8fbdd4b257fd872e7c5692e02a5e47f5
ccnmagnoo <crcamposn@gmail.com>
2023-08-31 12:30:00 -0400

    provider append

--- src/app/api/auth/[...nextAuth].ts
+++ src/app/api/auth/[...nextAuth].ts
@@ -1,10 +1,22 @@
 import NextAuth from 'next-auth/next';
 import FacebookProvider from 'next-auth/providers/facebook';
+import InstagramProvider from 'next-auth/providers/instagram';
+import GoogleProvider from 'next-auth/providers/google';
+
+//res https://www.youtube.com/watch?v=YCEnpcCYlyo
 
 export const authOptions = {
   //Configuration access providers
   providers: [
     FacebookProvider({
+      clientId: '',
+      clientSecret: '',
+    }),
+    InstagramProvider({
+      clientId: '',
+      clientSecret: '',
+    }),
+    GoogleProvider({
       clientId: '',
       clientSecret: '',
     }),
