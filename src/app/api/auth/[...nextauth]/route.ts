7be38672079cd910e020c551caa3df1d11496bdb
ccnmagnoo <crcamposn@gmail.com>
2023-09-01 09:41:43 -0400

    fb provider inti

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,11 +12,11 @@ const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    InstagramProvider({
-      clientId: '',
-      clientSecret: '',
-    }),
     FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    }),
+    InstagramProvider({
       clientId: '',
       clientSecret: '',
     }),
