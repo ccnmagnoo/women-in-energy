690f622ac4a8a07b56e79f028555d7902af96833
ccnmagnoo <crcamposn@gmail.com>
2023-09-11 13:11:08 -0300

    magnitude param, work size

--- src/Models/Input.ts
+++ src/Models/Input.ts
@@ -2,6 +2,7 @@ interface InputService {
   description: string;
   location: string;
   service: 'gas' | 'eli';
+  magnitude: 'home' | 'low_building' | 'high_building' | 'factory';
 }
 
 export type { InputService };
