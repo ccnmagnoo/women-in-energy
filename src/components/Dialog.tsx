5f94f2782d658c5e2dae98cc3936603b48b099a1
ccnmagnoo <crcamposn@gmail.com>
2023-09-12 15:37:41 -0300

    tag

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -2,9 +2,24 @@ import { InputService } from '@/Models/Input';
 import React from 'react';
 import { UseFormReturn } from 'react-hook-form';
 import styles from '@/components/Dialog.module.scss';
+import Image from 'next/image';
+
+import gas_ico from '@/app/static/gas-ico.svg';
+import eli_ico from '@/app/static/eli-ico.svg';
 
 export const Dialog = ({ form }: { form: UseFormReturn<Partial<InputService>> }) => {
   const { watch: obs } = form;
+
+  function renderIcon<T extends Partial<InputService>>(service: T['service']) {
+    switch (service) {
+      case 'gas':
+        return <Image key={service} className={styles.icon} src={gas_ico} alt='' />;
+      case 'eli':
+        return <Image key={service} className={styles.icon} src={eli_ico} alt='' />;
+      default:
+        return undefined;
+    }
+  }
 
   return (
     <>
@@ -16,6 +31,7 @@ export const Dialog = ({ form }: { form: UseFormReturn<Partial<InputService>> })
             <p>
               <span>en </span>
               {obs('location')}😍
+              <span className={styles.tag}>{renderIcon(form.watch('service'))}</span>
             </p>
           )}
         </article>
