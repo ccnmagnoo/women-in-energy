908082ccf9d0dfbb53113a31ad22219931e934f5
ccnmagnoo <crcamposn@gmail.com>
2023-09-13 14:48:58 -0300

    allowed source of images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const path = require('path');
 const nextConfig = {
   sassOptions: { includePaths: [path.join(__dirname, 'styles')] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
