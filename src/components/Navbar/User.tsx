eea0874adec37df86f58a1946eba84dbb18d849d
ccnmagnoo <crcamposn@gmail.com>
2023-09-15 12:40:46 -0300

    user card

--- src/components/Navbar/User.tsx
+++ src/components/Navbar/User.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export const User = ({ session }: { session: Session | null }) => {
   return (
     <section className={styles.user_container}>
-      Hola {session?.user?.name?.split(' ')[0]}
+      {session ? 'hola' : 'anónimo'} {session?.user?.name?.split(' ')[0]}
       <span className={styles.user_pic}>
         {session?.user?.image && (
           <Image width={50} height={50} src={session?.user?.image} alt='' />
