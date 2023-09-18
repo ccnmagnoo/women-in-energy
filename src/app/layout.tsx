19aa953b11495a4ab86d4128be1db72876fde23c
ccnmagnoo <crcamposn@gmail.com>
2023-09-18 20:00:01 -0300

    import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Providers from './Providers';
 import { Background } from '@/components/Background';
 import { Navbar } from '@/components/Navbar';
 import { URL } from 'url';
+import { ReactNode } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,7 @@ export const metadata: Metadata = {
     icon: '/src/app/static/mi-ico.png',
   },
 };
-function RootLayout({ children }: { children: React.ReactNode }) {
+function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
