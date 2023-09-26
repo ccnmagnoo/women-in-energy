49a4955a3d30e374289c5eae8e328349b683a84f
ccnmagnoo <crcamposn@gmail.com>
2023-09-26 16:34:59 -0300

    content type on form encoded

--- src/app/api/providers/route.ts
+++ src/app/api/providers/route.ts
@@ -7,24 +7,31 @@ async function handler(req: Request) {
   const service = searchParams.get('service');
 
   //fetch
-  const form = new FormData();
-  form.append('action', 'buscar');
-  form.append('rut', '14.189.412-9');
-  form.append('ambito', '1');
+  const payload = {
+    ambito: 1,
+    rut: '14.189.412-9',
+    accion: 'buscar',
+  };
+  const formData = new FormData();
+
+  const encoded = Object.entries(payload)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join('&');
+
+  console.log('form data to fetch', formData.get('ambito'));
 
   const data = await fetch(
     'https://wlhttp.sec.cl/validadorInstaladores/sec/consulta.do',
     {
       method: 'POST',
-      headers: { 'Content-Type': 'text/html;charset=utf-8' },
-      body: form,
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+      body: encoded,
     }
   );
-
-  console.log('fetching data☢️', data.body);
+  const hypertext = await data.text();
 
   //data res
-  return Res.json({ info: 'test' });
+  return Res.json({ response: hypertext });
 }
 
 export { handler as GET };
