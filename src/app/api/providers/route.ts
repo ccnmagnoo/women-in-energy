f75769746b9d8845e6f487b94971f804a588f938
ccnmagnoo <crcamposn@gmail.com>
2023-09-26 14:51:46 -0300

    test post data, route

--- src/app/api/providers/route.ts
+++ src/app/api/providers/route.ts
@@ -1,10 +1,30 @@
+import { NextApiRequest } from 'next';
 import { NextRequest as Req, NextResponse as Res } from 'next/server';
 
-function handler(req: Request) {
+async function handler(req: Request) {
   //retrieve params
   const { searchParams } = new URL(req.url);
   const service = searchParams.get('service');
-  return Res.json({ message: 'hello', service: service });
+
+  //fetch
+  const form = new FormData();
+  form.append('action', 'buscar');
+  form.append('rut', '14.189.412-9');
+  form.append('ambito', '1');
+
+  const data = await fetch(
+    'https://wlhttp.sec.cl/validadorInstaladores/sec/consulta.do',
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'text/html;charset=utf-8' },
+      body: form,
+    }
+  );
+
+  console.log('fetching data☢️', data.body);
+
+  //data res
+  return Res.json({ info: 'test' });
 }
 
 export { handler as GET };
