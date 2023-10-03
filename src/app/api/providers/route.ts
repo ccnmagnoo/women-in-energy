12b434b5e972bfcacad2b683cd5bf79efab91a31
ccnmagnoo <crcamposn@gmail.com>
2023-10-03 14:56:18 -0300

    api request

--- src/app/api/providers/route.ts
+++ src/app/api/providers/route.ts
@@ -1,17 +1,42 @@
 import { NextRequest as Req, NextResponse as Res } from 'next/server';
-import { ServiceUrl } from '../verification/Service';
 import { getService } from '../libs/validationLibs';
 import { InputService } from '@/Models/Input';
 import { getTerritory } from 'chilean-territory-code';
+import {
+  DocumentData,
+  collection,
+  getDoc,
+  getDocs,
+  limit,
+  query,
+  where,
+} from 'firebase/firestore';
+import { db } from '../libs/database';
 
 async function handler(req: Req, res: Res) {
   const { searchParams } = new URL(req.url);
-  const searchCriteria: Partial<InputService> = {
+  const toSearch: Partial<InputService> = {
     service: getService(searchParams.get('service')),
     location: getTerritory(searchParams.get('location') ?? undefined, 'city')?.name,
   };
 
-  return Res.json({ foo: 'bar' }, { status: 200 });
+  const providersQuery = query(
+    collection(db, '/energy_providers/source_providers/providers'),
+    where('personal.gender', '==', 'female'),
+    where('address.city', '==', toSearch.location),
+    where('license.service', '==', toSearch.service)
+  );
+
+  const snapshot = await getDocs(providersQuery);
+  const result: DocumentData = [];
+  snapshot.forEach((doc) => {
+    result.push(doc.data());
+  });
+
+  return Res.json({ search: toSearch, response: result }, { status: 200 });
 }
 
+/**
+ * search for professionals providers in firebase
+ */
 export { handler as GET };
