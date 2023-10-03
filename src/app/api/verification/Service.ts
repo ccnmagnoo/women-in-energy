281cfb21ad2df83655fd0a284a497e3a3b35a3a6
ccnmagnoo <crcamposn@gmail.com>
2023-10-03 09:13:07 -0300

    payload type

--- src/app/api/verification/Service.ts
+++ src/app/api/verification/Service.ts
@@ -1,5 +1,11 @@
 import { InputService } from '@/Models/Input';
 
+/**
+ * type used for SEC platform post request payload
+ * @param ambito: 1: electricity service & 2: gas services
+ * @param rut, social security number with dotted format 10.000.000-0,
+ * @param accion, 'buscar' literal to request data
+ */
 export type SecPayload = {
   ambito: 1 | 2;
   rut: string;
