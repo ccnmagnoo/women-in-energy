8d744d8ce67849bc277429c72e6e44feb1187670
ccnmagnoo <crcamposn@gmail.com>
2023-10-03 09:00:59 -0300

    get url params validation

--- src/app/api/providers/route.ts
+++ src/app/api/providers/route.ts
@@ -1,6 +1,16 @@
 import { NextRequest as Req, NextResponse as Res } from 'next/server';
+import { ServiceUrl } from '../verification/Service';
+import { getService } from '../libs/validationLibs';
+import { InputService } from '@/Models/Input';
+import { getTerritory } from 'chilean-territory-code';
 
 async function handler(req: Req, res: Res) {
+  const { searchParams } = new URL(req.url);
+  const searchCriteria: Partial<InputService> = {
+    service: getService(searchParams.get('service')),
+    location: getTerritory(searchParams.get('location') ?? undefined, 'city')?.name,
+  };
+
   return Res.json({ foo: 'bar' }, { status: 200 });
 }
 
