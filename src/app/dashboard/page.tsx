c25a3b0b5643382fef049a14689e285334507a1f
ccnmagnoo <crcamposn@gmail.com>
2023-10-06 13:26:58 -0300

    fetch data from api, error with url parse, still works

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,27 +1,57 @@
 'use client';
-import { useSearchParams } from 'next/navigation';
+import { ReadonlyURLSearchParams, useSearchParams } from 'next/navigation';
 import { InputService } from '@/Models/Input';
+import { useContext, useEffect, useState } from 'react';
 
 const Dashboard = () => {
   const by_url = useSearchParams();
-  const service: Partial<InputService> = {
-    description: by_url.get('description') || undefined,
-    service: ((by_url.get('service') || undefined) as 'eli') || 'gas',
-    location: by_url.get('location') || undefined,
-    magnitude:
-      ((by_url.get('magnitude') || undefined) as 'home') ||
-      'low_building' ||
-      'high_building' ||
-      'factory',
+  // const service: Partial<InputService> = {
+  //   description: by_url.get('description') || undefined,
+  //   service: (by_url.get('service') || undefined) as InputService['service'],
+  //   location: by_url.get('location') || undefined,
+  //   magnitude: (by_url.get('magnitude') || undefined) as InputService['magnitude'],
+  // };
+
+  const searchParams: PartialStringified<InputService> = {
+    description: undefined,
+    service: undefined,
+    location: undefined,
+    magnitude: undefined,
   };
+
+  const build: Record<string, string | undefined> = {};
+  Object.keys(searchParams).forEach((key) => {
+    build[key] = by_url.get(key) || undefined;
+  });
+
+  //providers hooks
+  const [providers, setProviders] = useState();
+  fetchProviders(build);
+
   return (
     <main>
       dashboard page <br />
-      {service.location} <br />
-      {service.description} <br />
-      {service.service}
+      {build.location} <br />
+      {build.description} <br />
+      {build.service}
     </main>
   );
 };
+
+async function fetchProviders<T extends Record<string, string | undefined>, K>(
+  params: T
+) {
+  try {
+    const api =
+      '/api/providers?' +
+      Object.entries(params)
+        .map(([key, value]) => `${key}=${value ?? ''}`)
+        .join('&');
+
+    fetch(api);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default Dashboard;
