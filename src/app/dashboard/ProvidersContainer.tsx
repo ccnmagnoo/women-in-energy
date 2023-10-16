9450ec6370dcbe451fd09cdf863be53f53b67c8b
ccnmagnoo <crcamposn@gmail.com>
2023-10-16 08:47:16 -0300

    result territorial section title

--- src/app/dashboard/ProvidersContainer.tsx
+++ src/app/dashboard/ProvidersContainer.tsx
@@ -25,24 +25,43 @@ export const ProvidersContainer = ({
       <section className={style.body}>
         {res &&
           Object.entries(res?.response).map(([scope, listProviders]) => {
-            return (
-              <article key={scope} className={style.territorialSection}>
-                <h3>
-                  {scope} ({listProviders.length})
-                </h3>
-                <ul>
-                  {listProviders.map((provider) => {
-                    return (
-                      <li key={provider.uuid} data-scope={scope}>
-                        <ProviderCard provider={provider} scope={scope as Territory} />
-                      </li>
-                    );
-                  })}
-                </ul>
-              </article>
-            );
+            if (listProviders.length) {
+              //filter empty list
+              return (
+                <article key={scope} className={style.territorialSection}>
+                  <h3>
+                    {scopeSectionTitle(scope as Territory)} ({listProviders.length})
+                  </h3>
+                  <ul>
+                    {listProviders.map((provider) => {
+                      return (
+                        <li key={provider.uuid} data-scope={scope}>
+                          <ProviderCard provider={provider} scope={scope as Territory} />
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </article>
+              );
+            }
           })}
       </section>
     </section>
   );
 };
+
+function scopeSectionTitle(scope: Territory) {
+  switch (scope) {
+    case 'city': {
+      return 'en tu comuna';
+    }
+    case 'province': {
+      return 'a tu alrededor';
+    }
+    case 'region': {
+      return 'en tu región';
+    }
+    default:
+      return undefined;
+  }
+}
