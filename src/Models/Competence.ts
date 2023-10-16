f860ce3f4a1cb3321be45e2d2dd57a1f83094325
ccnmagnoo <crcamposn@gmail.com>
2023-10-16 16:16:26 -0300

    instances of Competence

--- src/Models/Competence.ts
+++ src/Models/Competence.ts
@@ -1,19 +1,37 @@
 //ref https://www.sec.cl/area-instaladores/instaladores-electricos/#1582631689365-5ce39298-c21f
 
-import { Service } from './Providers';
+import { Eli, Gas, License, Service } from './Providers';
 
 type Voltage = 'high' | 'low';
 
-interface Installs {
-  name: string;
+interface Install {
+  technology: string;
   description?: string;
   limitSize?: number;
   feederSize?: number;
-  unit: 'kW' | 'Kg' | 'Lt';
+  unit: 'kW' | 'Kg';
 }
 
 interface Competence {
-  service: Service;
   fireDanger: boolean;
-  installation: Installs[];
+  installation: Install[];
 }
+
+type CompetenceList<S extends Service> = {
+  [K in License<S>['category']]: Competence;
+};
+
+const EliCompetence: CompetenceList<Eli> = {
+  A: { fireDanger: false, installation: [] },
+  B: { fireDanger: false, installation: [] },
+  C: { fireDanger: false, installation: [] },
+  D: { fireDanger: false, installation: [] },
+};
+
+const GasCompetence: CompetenceList<Gas> = {
+  '1': { fireDanger: false, installation: [] },
+  '2': { fireDanger: false, installation: [] },
+  '3': { fireDanger: false, installation: [] },
+  '4': { fireDanger: false, installation: [] },
+  '1 y 4': { fireDanger: false, installation: [] },
+};
