ae1689746b6e5ab18f9158501749308db6555210
ccnmagnoo <crcamposn@gmail.com>
2023-10-17 13:01:58 -0300

    tag container for future content, about license grades

--- src/app/dashboard/LicenseTag.tsx
+++ src/app/dashboard/LicenseTag.tsx
@@ -6,13 +6,15 @@ export function LicenseTag<S extends Service>({ license }: { license: License<S>
   const data = license;
 
   return (
-    <div className={style.tag}>
-      <div className={style.upper}>
-        <span className={style.title}>clase</span>
-      </div>
-      <div className={style.lower}>
-        <span className={style.category}>{license.category}</span>
-        <span className={style.ico}>{license.service === 'eli' ? '⚡' : '🔥'}</span>
+    <div className={style.container}>
+      <div className={style.tag}>
+        <div className={style.upper}>
+          <span className={style.title}>clase</span>
+        </div>
+        <div className={style.lower}>
+          <span className={style.category}>{license.category}</span>
+          <span className={style.ico}>{license.service === 'eli' ? '⚡' : '🔥'}</span>
+        </div>
       </div>
     </div>
   );
