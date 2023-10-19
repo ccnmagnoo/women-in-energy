7a168c2e7adcb90251311c5f1048e64b84652ffa
ccnmagnoo <crcamposn@gmail.com>
2023-10-19 14:31:51 -0300

    message url encode

--- src/app/dashboard/Contact.tsx
+++ src/app/dashboard/Contact.tsx
@@ -6,6 +6,7 @@ import style from './Contact.module.scss';
 import { Provider, Service } from '@/Models/Providers';
 import { InputService } from '@/Models/Input';
 import { useSearchParams } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 export const Contact = <S extends Service>({ provider }: { provider: Provider<S> }) => {
   const by_url = useSearchParams();
@@ -16,23 +17,36 @@ export const Contact = <S extends Service>({ provider }: { provider: Provider<S>
     magnitude: undefined,
   };
 
+  const session = useSession().data;
+
   const reqParams: Record<string, string | undefined> = {};
   Object.keys(toSearch).forEach((key) => {
     reqParams[key] = by_url.get(key) || undefined;
   });
 
+  const serviceMessage = `Junto con saludar ${provider.personal.name}%0D
+  Necesito que me pueda colaborar con la siguiente tarea:%0D
+  ${reqParams.description} ,espero que puedas ayudarme.%0D%0D
+  Mis datos son:%0D
+  ${session?.user?.name}, mi email es: ${session?.user?.email}.%0D%0D
+  Muchas gracias de antemano🖐️.%0D%0D
+  Promoviendo a las mujeres técnicas de Chile:%0D
+  http://instaladoras.web.app de minenergia.cl
+  `;
+
   return (
     <section className={style.contact}>
       {/* https://faq.whatsapp.com/5913398998672934 */}
-      <a href='' target='_blank'>
+      <a
+        href={`https://wa.me/${provider.contact.movil}?text=${serviceMessage}`}
+        target='_blank'
+      >
         <Image src={sms} alt={''}></Image>
       </a>
       <a
-        href={`mailto:${
-          provider.contact.email
-        }?subject=${'requerimiento de servicio SEC'}&body=Junto con saludar, necesito ${
-          reqParams.description
-        }`}
+        href={`mailto:${provider.contact.email}?subject=Requerimiento de servicio ${
+          provider.license.service === 'eli' ? 'eléctrico' : ' de Gas'
+        } SEC&body=${serviceMessage}`}
         target='_blank'
       >
         <Image src={email} alt={''}></Image>
