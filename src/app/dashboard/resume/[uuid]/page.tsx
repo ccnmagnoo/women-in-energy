e59e27bca74513edfdfa9f73ce6e784895d23cba
ccnmagnoo <crcamposn@gmail.com>
2023-10-25 17:59:53 -0300

    big provider card

--- src/app/dashboard/resume/[uuid]/page.tsx
+++ src/app/dashboard/resume/[uuid]/page.tsx
@@ -2,6 +2,9 @@
 import React, { useEffect, useState } from 'react';
 import style from './Resume.module.scss';
 import { Provider, ApiResponse, Service } from '@/Models/Providers';
+import { LicenseTag } from '../../LicenseTag';
+import Image from 'next/image';
+import avatar from '@/app/static/woman-ico.svg';
 
 function ProviderResume<S extends Service>({ params }: { params: { uuid: string } }) {
   const [provider, setProvider] = useState<undefined | ApiResponse<Provider<S>>>(
@@ -10,6 +13,7 @@ function ProviderResume<S extends Service>({ params }: { params: { uuid: string 
 
   useEffect(() => {
     async function fetchProvider() {
+      //https://stackoverflow.com/questions/55690143/what-is-the-difference-between-env-local-and-env-development-local
       const query = await fetch('/api/personal?uuid=' + params.uuid);
       const data = (await query.json()) as ApiResponse<Provider<S>>;
       //                                        ^?
@@ -21,7 +25,17 @@ function ProviderResume<S extends Service>({ params }: { params: { uuid: string 
   return (
     <main className={style.main}>
       <section className={style.container}>
-        {provider?.response.rut} {provider?.response.personal.name}
+        <div className={style.top}>
+          <article className={style.avatarFrame}>
+            {provider && <LicenseTag provider={provider?.response} />}
+            <section className={style.avatar}>
+              <Image src={avatar} alt={''} />
+            </section>
+          </article>
+          <article className={style.personal}>informacin persona</article>
+        </div>
+
+        <div className={style.bottom}>contacto</div>
       </section>
     </main>
   );
