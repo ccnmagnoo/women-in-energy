7fbc957ca31e41b9136cf3daa63ec3c836ab375d
ccnmagnoo <crcamposn@gmail.com>
2023-11-03 16:51:45 -0300

    dynamic contact service

--- src/app/dashboard/resume/[uuid]/BigContact.tsx
+++ src/app/dashboard/resume/[uuid]/BigContact.tsx
@@ -1,11 +1,28 @@
-import { Provider, Service } from '@/Models/Providers';
+import { Contact, Provider, Service } from '@/Models/Providers';
 import React from 'react';
 import style from './Resume.module.scss';
+import { BigContactMedia } from './BigContactMedia';
 
 export const BigContact = <S extends Service>({
   provider,
 }: {
   provider?: Provider<S>;
 }) => {
-  return <section className={style.contact}>Contact</section>;
+  return (
+    <section className={style.contact}>
+      <ul>
+        {provider &&
+          Object.entries(provider?.contact).map((par) => {
+            const [media, identifier] = par;
+            return (
+              <BigContactMedia
+                key={media}
+                media={media as keyof Contact}
+                identifier={identifier}
+              />
+            );
+          })}
+      </ul>
+    </section>
+  );
 };
