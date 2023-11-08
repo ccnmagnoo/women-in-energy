1264fae07f456ef142ae5d91b25f6b705ee54992
ccnmagnoo <crcamposn@gmail.com>
2023-11-08 17:17:57 -0300

    deploy config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 const nextConfig = {
+  distDir: 'build',
   sassOptions: { includePaths: [path.join(__dirname, 'styles')] },
+  output: 'export',
   images: {
     remotePatterns: [
       {
