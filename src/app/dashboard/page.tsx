709067bbdb74162b2d173c6c758e0f3849a87c8f
ccnmagnoo <crcamposn@gmail.com>
2023-11-09 15:11:36 -0300

    get server side next function

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from 'react';
 import { Eli, Gas, ApiResponse, SearchResponse, Provider } from '@/Models/Providers';
 import dynamic from 'next/dynamic';
 import { Loading } from '@/components/Loading';
+import { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import { KeyObject } from 'crypto';
 
 const DynamicProviderContainer = dynamic(
   () => import('./ProvidersContainer').then((mod) => mod.ProvidersContainer),
@@ -37,8 +39,8 @@ const Dashboard = () => {
 
   useEffect(() => {
     async function fetch() {
-      const data = await fetchProviders(buildParams);
-      setProviders(data);
+      //const data = await fetchProviders(buildParams);
+      //setProviders(data);
     }
 
     fetch();
@@ -52,20 +54,41 @@ const Dashboard = () => {
   );
 };
 
-async function fetchProviders<T extends {}>(params: T) {
-  try {
-    const api =
-      '/api/providers?' +
-      Object.entries(params)
-        .map(([key, value]) => `${key}=${value ?? ''}`)
-        .join('&');
+/**
+ * How to deploy at hosting Next JS on firebase
+ * https://firebase.google.com/docs/hosting/frameworks/nextjs?hl=es-419
+ * next js docs
+ * https://nextjs.org/docs/pages/api-reference/functions/get-server-side-props
+ * video
+ * https://youtu.be/TJqJ-WY3XxA
+ */
 
-    const data = await fetch(api);
+export const getServerSideProps = (async (context) => {
+  //url params
+  const params: PartialStringified<InputService> = {
+    description: undefined,
+    service: undefined,
+    location: undefined,
+    magnitude: undefined,
+  };
 
-    return data.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
+  const build: Record<string, string | undefined> = {};
+  Object.keys(params).forEach((key) => {
+    const get = context.query[key];
+    build[key] = get as string;
+  });
+
+  //url fetch
+  const request_params = Object.entries(build)
+    .map(([key, value]) => key + '=' + value)
+    .join('&');
+  const request_url = '/api/provider?' + request_params;
+
+  const res = await fetch(request_params);
+  const repo = await res.json();
+  return { props: { repo } };
+}) satisfies GetServerSideProps<{
+  repo: number;
+}>;
 
 export default Dashboard;
