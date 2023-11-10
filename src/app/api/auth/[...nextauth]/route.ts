096626b8e5ee3fb5f4301bff22dadc93b4f94ebd
ccnmagnoo <crcamposn@gmail.com>
2023-11-10 11:56:26 -0300

    clean

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,7 @@ import NextAuth from 'next-auth/next';
 // import FacebookProvider from 'next-auth/providers/facebook';
 // import InstagramProvider from 'next-auth/providers/instagram';
 import GoogleProvider from 'next-auth/providers/google';
-
 import { AuthOptions, Theme } from 'next-auth';
-import app_logo from '@/app/static/mi-logo.svg';
 
 //res https://www.youtube.com/watch?v=YCEnpcCYlyo
 const theme: Theme = {
