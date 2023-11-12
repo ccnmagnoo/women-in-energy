eb3a1af44e408d15237a6734a85d4e7649b1cfba
ccnmagnoo <ccampos@dvt.cl>
2023-11-12 19:13:08 -0300

    fix potential at() issue a L45

--- src/app/dashboard/Validation.tsx
+++ src/app/dashboard/Validation.tsx
@@ -30,18 +30,19 @@ function Validation<S extends Service>({ provider }: { provider: Provider<S> }) 
     <>
       {validation && (
         <section className={style.stamp}>
-          {validation &&
+          {
             //FIXME: validation is nullable, so y async load drops Null pointer exception error.
-            (validation?.response?.at(-1) === 'VIGENTE' ? (
+            validation?.response?.at(-1) === 'VIGENTE' ? (
               <Image src={validStamp} alt='' />
             ) : (
               <Image src={invalidStamp} alt='' />
-            ))}
+            )
+          }
 
           <article className={style.dialog}>
             <a href={validation.source} target='_blank'>
               <p>licencia</p>
-              <p>{validation.response.at(-1)?.toLowerCase()}</p>
+              <p>{validation?.response?.at(-1)?.toLowerCase()}</p>
             </a>
           </article>
         </section>
