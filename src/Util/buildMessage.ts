21047accc77f536e3e3cf4a664537e11c159f3a3
ccnmagnoo <ccampos@dvt.cl>
2023-11-12 19:51:43 -0300

    update media message text

--- src/Util/buildMessage.ts
+++ src/Util/buildMessage.ts
@@ -7,13 +7,16 @@ async function buildMessage<S extends Service>(
 ) {
   const session = await getSession();
   const serviceMessage = `Junto con saludar ${provider?.personal.name}%0D
-    Necesito que me pueda colaborar con la siguiente tarea:%0D
+    Necesito que pueda cotizar la siguiente tarea:%0D
     ${params?.description ?? 'servicios profesionales'} ,espero que puedas ayudarme.%0D%0D
     Mis datos son:%0D
-    ${session?.user?.name}, mi email es: ${session?.user?.email}.%0D%0D
-    Muchas gracias.%0D%0D
-    Promoviendo a las mujeres técnicas de Chile:%0D
-    http://instaladoras.web.app del minenergia.cl
+    ${session?.user?.name}, mi email es: ${
+    session?.user?.email ?? '☝ este mismo contacto'
+  }.%0D%0D
+    Muchas gracias de antemano 😊.%0D%0D
+    Mensaje enviado mediante http://instaladoras.vercel.app%0D
+    del Ministerio de Energía:%0D
+    Apoyando a las mujeres profesionales de Chile 💪.
     ` as const;
 
   return serviceMessage;
