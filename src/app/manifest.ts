507a8c2f199e4d33b7f2a0e30253d465d476cfa5
ccnmagnoo <crcamposn@gmail.com>
2023-11-13 11:34:01 -0300

    metadata update

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -5,5 +5,20 @@ export default function manifest(): MetadataRoute.Manifest {
     name: 'instaladoras App',
     short_name: 'instaladoras App',
     description: 'Promoviendo a mujeres profesionales instaladoras de Gas y Electricidad',
+    start_url: '/',
+    background_color: '#fff',
+    theme_color: 'light',
+    icons: [{ src: '/favicon.ico', sizes: 'any', type: 'image/x-icon' }],
+    related_applications: [
+      {
+        platform: 'SEC',
+        url: 'http://www.sec.cl',
+      },
+      {
+        platform: 'SEC validation',
+        url: 'https://wlhttp.sec.cl',
+      },
+      { platform: 'Ministerio de Energía', url: 'https://energia.gob.cl' },
+    ],
   };
 }
