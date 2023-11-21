4bbbd5d9656631b2593de68af6814b4824931aea
ccnmagnoo <ccampos@dvt.cl>
2023-11-21 20:56:27 -0300

    using api validation with no-cors mode

--- src/app/dashboard/Validation.tsx
+++ src/app/dashboard/Validation.tsx
@@ -9,41 +9,41 @@ import Image from 'next/image';
 import { useCertification } from './certification/useCertification';
 
 function Validation<S extends Service>({ provider }: { provider: Provider<S> }) {
-  const certification: ValidationRequest | undefined = useCertification({
-    service: { service: provider.license.service, rut: provider.rut },
-  });
+  // const certification: ValidationRequest | undefined = useCertification({
+  //   service: { service: provider.license.service, rut: provider.rut },
+  // });
 
-  // const [validation, setValidation] = useState<ValidationRequest | undefined>(undefined);
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       console.log(
-  //         'requesting API verification:',
-  //         provider.license.service,
-  //         provider.rut
-  //       );
-  //       const query = fetch(
-  //         `/api/verification?service=${provider.license.service}&rut=${provider.rut}`
-  //       );
-  //       const response = await query;
-  //       const result: ValidationRequest | undefined = await response.json();
-  //       console.log('api validation result: ', result?.response);
-  //       setValidation(result);
-  //     } catch (error) {
-  //       console.error('error detail', error);
-  //     }
-  //   };
+  const [validation, setValidation] = useState<ValidationRequest | undefined>(undefined);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log(
+          'requesting API verification:',
+          provider.license.service,
+          provider.rut
+        );
+        const query = fetch(
+          `/api/verification?service=${provider.license.service}&rut=${provider.rut}`
+        );
+        const response = await query;
+        const result: ValidationRequest | undefined = await response.json();
+        console.log('api validation result: ', result?.response);
+        setValidation(result);
+      } catch (error) {
+        console.error('error detail', error);
+      }
+    };
 
-  //   fetchData();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
-      {certification?.response && (
+      {validation?.response && (
         <section className={style.stamp}>
           {
             //FIXME: validation is nullable, so y async load drops Null pointer exception error.
-            certification?.response?.at(-1) === 'VIGENTE' ? (
+            validation?.response?.at(-1) === 'VIGENTE' ? (
               <Image src={validStamp} alt='valid' />
             ) : (
               <Image src={nullStamp} alt='void' />
@@ -51,10 +51,10 @@ function Validation<S extends Service>({ provider }: { provider: Provider<S> }) 
           }
 
           <article className={style.dialog}>
-            <a href={certification.source} target='_blank'>
+            <a href={validation.source} target='_blank'>
               {/* cspell:disable */}
               <p>licencia</p>
-              <p>{certification?.response?.at(-1)?.toLowerCase()}</p>
+              <p>{validation?.response?.at(-1)?.toLowerCase()}</p>
             </a>
           </article>
         </section>
